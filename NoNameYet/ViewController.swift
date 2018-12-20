9fc1cddda2196807000cc63c0a273c3a62890466
wongandydev <wongandy97@gmail.com>
2018-12-20 16:22:00 -0500

    first commit

---
+++ NoNameYet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoNameYet
+//
+//  Created by Andy Wong on 12/20/18.
+//  Copyright © 2018 Andy Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
