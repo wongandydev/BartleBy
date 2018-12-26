22788ef1147e3585c6bfa5e7f100786bde7eb06d
wongandydev <wongandy97@gmail.com>
2018-12-25 22:48:40 -0500

    add logic for notification authorization

--- BartleBy/AppDelegate.swift
+++ BartleBy/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let unc = UNUserNotificationCenter.current()
+        unc.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            
+        }
         return true
     }
 
