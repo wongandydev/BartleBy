efd4db91149a17c92c35d1f51b5e66568e3b0bdb
wongandydev <wongandy97@gmail.com>
2018-12-25 22:49:28 -0500

    add logic and view to allowing user to pick notifcation time

--- BartleBy/ViewControllers/NotificationViewController.swift
+++ BartleBy/ViewControllers/NotificationViewController.swift
@@ -6,4 +6,49 @@
 //  Copyright © 2018 Andy Wong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import UserNotifications
+
+class NotificationViewController: UIViewController {
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var currentNotificationSettingLabel: UILabel!
+    @IBOutlet weak var changeNotifcationDatePicker: UIDatePicker!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+
+        content.title = "Another day of writing!"
+        content.subtitle = "This is daily message"
+        content.body = "Have a good day"
+        content.sound = UNNotificationSound.default
+
+        let setTime = Helper.sharedInstance.convertAMto24(date: changeNotifcationDatePicker.date)
+        var date = DateComponents()
+        date.hour = Int(setTime.components(separatedBy: ":")[0])
+        date.minute = Int(setTime.components(separatedBy: ":")[1])
+
+        let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        notificationCenter.removeAllPendingNotificationRequests()
+        notificationCenter.add(request)
+        UserDefaults.standard.set(Helper.sharedInstance.convert24toAM(date: changeNotifcationDatePicker.date), forKey: "notificationSetting")
+        currentNotificationSettingLabel.text = UserDefaults.standard.object(forKey: "notificationSetting") as! String
+        cancelButton.setTitle("Done", for: .normal)
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        changeNotifcationDatePicker.datePickerMode = .time
+        changeNotifcationDatePicker.date = Helper.sharedInstance.stringToDate(date: UserDefaults.standard.object(forKey: "notificationSetting") as! String)
+        currentNotificationSettingLabel.text = UserDefaults.standard.object(forKey: "notificationSetting") == nil ? "No Notification": UserDefaults.standard.object(forKey: "notificationSetting") as! String
+    }
+}
