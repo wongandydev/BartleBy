c19e277a0ae24b540feb5a0b6c3524c89e67f2e8
wongandydev <wongandy97@gmail.com>
2018-12-29 22:20:08 -0500

    add new extension and VC

---
+++ BartleBy/NavigationController+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationController+Extension.swift
+//  BartleBy
+//
+//  Created by Andy Wong on 12/29/18.
+//  Copyright © 2018 Andy Wong. All rights reserved.
+//
+
+import Foundation
---
+++ BartleBy/UIViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController+Extensions.swift
+//  BartleBy
+//
+//  Created by Andy Wong on 12/28/18.
+//  Copyright © 2018 Andy Wong. All rights reserved.
+//
+
+import Foundation
