15c3d4d3f3f297b5bee13d67d920e690e6294632
wongandydev <wongandy97@gmail.com>
2018-12-29 22:25:57 -0500

    add transparent nav bar

--- BartleBy/NavigationController+Extension.swift
+++ BartleBy/NavigationController+Extension.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2018 Andy Wong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UINavigationController {
+    func transparentNavBar() {
+        self.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationBar.shadowImage = UIImage()
+        self.navigationBar.isTranslucent = true
+    }
+}
