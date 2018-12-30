9e33dc17db3e59b6b22620f0dc6c07893e95559d
wongandydev <wongandy97@gmail.com>
2018-12-29 22:25:41 -0500

    add alertmessage func

--- BartleBy/UIViewController+Extensions.swift
+++ BartleBy/UIViewController+Extensions.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2018 Andy Wong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func alertMessage(title: String, message: String) {
+        let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+        blurView.frame = self.view.frame
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { action in
+            blurView.removeFromSuperview()
+        }))
+        
+        self.view.addSubview(blurView)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func cancelButton(title: String, color: UIColor) {
+        let cancelButton = UIButton(frame: CGRect(x: 0, y: self.view.frame.height-144, width: self.view.frame.width, height: 64))
+        cancelButton.setTitle(title, for: .normal)
+        cancelButton.backgroundColor = color
+        cancelButton.addTarget(self, action: #selector(dismissVC), for: .touchUpInside)
+        
+        self.view.addSubview(cancelButton)
+    }
+    
+    @objc func dismissVC() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
